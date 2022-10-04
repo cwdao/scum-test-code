caad6a1f5e022b6c549b2c7681e0bcd9450a1ec4
Titan Yuan <tryuan99@gmail.com>
2022-10-03 18:28:43 -0700

    Sample 60000 times in quick succession

--- scm_v3c/applications/sensor_adc/sensor_adc.c
+++ scm_v3c/applications/sensor_adc/sensor_adc.c
@@ -8,6 +8,9 @@
 #include "optical.h"
 #include "scm3c_hw_interface.h"
 
+// Number of ADC samples.
+#define NUM_ADC_SAMPLES 60000
+
 // ADC configuration.
 static const adc_config_t g_adc_config = {
     .reset_source = ADC_RESET_SOURCE_FSM,
@@ -17,11 +20,14 @@ static const adc_config_t g_adc_config = {
     .settling_time = 0,
     .bandgap_reference_tuning_code = 1,
     .const_gm_tuning_code = 0xFF,
-    .vbat_div_4_enabled = true,
+    .vbat_div_4_enabled = false,
     .ldo_enabled = true,
-    .input_mux_select = ADC_INPUT_MUX_SELECT_V_BAT_DIV_4,
+    .input_mux_select = ADC_INPUT_MUX_SELECT_EXTERNAL_SIGNAL,
     .pga_bypass = true,
 };
+
+// ADC samples.
+static uint8_t g_adc_samples[NUM_ADC_SAMPLES];
 
 int main(void) {
     initialize_mote();
@@ -37,19 +43,33 @@ int main(void) {
     crc_check();
     perform_calibration();
 
-    while (true) {
+    // Warm up the ADC reads.
+    for (uint32_t i = 0; i < 5; ++i) {
+        adc_trigger();
+        while (!g_adc_output.valid)
+            ;
+    }
+
+    // Output the ADC convert signal over GPIO 2 in bank 9.
+    GPO_control(9, 9, 9, 9);
+    GPI_enables(0x0000);
+    GPO_enables(0xFFFF);
+
+    for (uint32_t i = 0; i < NUM_ADC_SAMPLES; ++i) {
         // Trigger an ADC read.
-        printf("Triggering ADC.\n");
         adc_trigger();
         while (!g_adc_output.valid) {
         }
         if (!g_adc_output.valid) {
             printf("ADC output should be valid.\n");
         }
-        printf("ADC output: %u\n", g_adc_output.data);
 
-        // Wait a bit.
-        for (uint32_t i = 0; i < 1000000; ++i) {
-        }
+        g_adc_samples[i] = (uint8_t)g_adc_output.data;
     }
+
+    printf("Done with %u ADC samples.\n", NUM_ADC_SAMPLES);
+    for (uint32_t i = 0; i < NUM_ADC_SAMPLES; ++i) {
+        printf("%u\n", g_adc_samples[i]);
+    }
+    printf("Done.\n");
 }
