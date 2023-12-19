12ce673d086ca463ccc0b954964617ad8895458c
Titan Yuan <tryuan99@gmail.com>
2023-12-18 16:24:10 -0800

    Generate a PWM ramp from 0 V to 0.75 V with a ramp rate of 0.175 V/s

--- scm_v3c/applications/sensor_adc/sensor_adc.c
+++ scm_v3c/applications/sensor_adc/sensor_adc.c
@@ -9,10 +9,16 @@
 #include "scm3c_hw_interface.h"
 
 // Number of pulses per PWM ramp.
-#define NUM_PULSES_PER_RAMP 600
+#define NUM_PULSES_PER_RAMP 3500
 
 // Number of cycles per pulse.
-#define NUM_CYCLES_PER_PULSE 1200
+#define NUM_CYCLES_PER_PULSE 800
+
+// Fraction increment per pulse.
+#define FRACTION_INCREMENT_PER_PULSE 8
+
+// If true, take measurements with the ADC during the ramp.
+#define ADC_ENABLED false
 
 // ADC configuration.
 static const adc_config_t g_adc_config = {
@@ -29,6 +35,7 @@ static const adc_config_t g_adc_config = {
     .pga_bypass = true,
 };
 
+#if ADC_ENABLED
 // ADC data for a single PWM ramp.
 static uint16_t g_adc_data_for_single_ramp[NUM_PULSES_PER_RAMP][2];
 
@@ -37,6 +44,7 @@ static inline void read_adc_output(void) {
     while (!g_adc_output.valid) {
     }
 }
+#endif  // ADC_ENABLED
 
 int main(void) {
     initialize_mote();
@@ -67,38 +75,46 @@ int main(void) {
         printf("Starting a new PWM ramp.\n");
         // Generate a PWM ramp.
         for (uint32_t i = 0; i < NUM_PULSES_PER_RAMP; ++i) {
+#if ADC_ENABLED
             // Read the ADC.
             read_adc_output();
             g_adc_data_for_single_ramp[i][0] = g_adc_output.data;
+#endif  // ADC_ENABLED
 
             // Start the pulse.
             GPIO_REG__OUTPUT |= 0x0001;
 
             // Wait for the pulse width.
-            for (uint32_t j = 0; j < i; ++j) {
+            for (uint32_t j = 0; j < i / FRACTION_INCREMENT_PER_PULSE; ++j) {
             }
 
+#if ADC_ENABLED
             // Read the ADC.
             read_adc_output();
             g_adc_data_for_single_ramp[i][1] = g_adc_output.data;
+#endif  // ADC_ENABLED
 
             // Lower the pulse.
             GPIO_REG__OUTPUT &= ~0x0001;
 
             // Wait until the next pulse.
-            for (uint32_t j = 0; j < NUM_CYCLES_PER_PULSE - i; ++j) {
+            for (uint32_t j = 0;
+                 j < NUM_CYCLES_PER_PULSE - i / FRACTION_INCREMENT_PER_PULSE;
+                 ++j) {
             }
         }
 
+#if ADC_ENABLED
         // Print the ADC data for the latest PWM ramp.
         printf("ADC data for PWM ramp:\n");
         for (uint32_t i = 0; i < NUM_PULSES_PER_RAMP; ++i) {
             printf("%d %d\n", g_adc_data_for_single_ramp[i][0],
                    g_adc_data_for_single_ramp[i][1]);
         }
+#endif  // ADC_ENABLED
 
         // Wait a bit.
-        for (uint32_t i = 0; i < 20000000; ++i) {
+        for (uint32_t i = 0; i < 10000000; ++i) {
         }
     }
 }
